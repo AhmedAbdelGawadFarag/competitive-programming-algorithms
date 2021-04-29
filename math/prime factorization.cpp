d7e408b3d82310c8a7c13f3f7227a216b02b1932
ahmed beeka <ahmed.abdelgawad.farag@gmail.com>
2021-04-29 05:55:32 +0200

    organize folders

---
+++ math/prime factorization.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, int>> PrimeFactors(int n) {
+
+    vector<pair<int, int>> temp;
+    for (int i = 2; i <= n / i; i++) {
+        int cnt = 0;
+        while (n % i == 0) {
+            cnt++;
+            n /= i;
+        }
+        if (cnt > 0)
+            temp.push_back({i, cnt});
+
+    }
+    if (n > 1)temp.push_back({n, 1});
+    return temp;
+}
+
+int main() {
+    int d;
+    cin >> d;
+
+    vector<pair<int, int>> temp = PrimeFactors(d);
+
+    for (auto p :temp)cout << p.first << " " << p.second << endl;
+
+}
