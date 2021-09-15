28a73b92d2ef3dff7ac3aa90f3048d0334e278d5
ahmed abdelgawad <ahmed.abdelgawad.farag@gmail.com>
2021-09-15 08:03:35 +0200

    clean some files

--- math/modifiedSieve.cpp
+++ math/modifiedSieve.cpp
@@ -8,37 +8,49 @@ const int N = 2e5 + 9;
 
 int factor[N];
 
-void modifiedSieve() {
+void modifiedSieve()
+{
     iota(factor, factor + N, 0);
     factor[0] = factor[1] = 1;
 
-    for (int i = 2; i < N / i; i += 1 + (1 & i)) {
-        if (factor[i] == i) {// prime
-            for (int j = i * i; j < N; j += i) {
-                if (factor[j] == j) {//not visited still have his factor
+    for (int i = 2; i < N / i; i += 1 + (1 & i))
+    {
+        if (factor[i] == i)
+        { // prime
+            for (int j = i * i; j < N; j += i)
+            {
+                if (factor[j] == j)
+                { //not visited still have his factor
                     factor[j] = i;
                 }
             }
         }
     }
-
 }
 
-void factfast(int x) {// get prime factoraztion in o(log n)
+void factfast(int x)
+{ // get prime factoraztion in o(log n)
 
-    while (x > 1) {
-        cout << factor[x] << endl;
-        x /= factor[x];
+    while (x > 1)
+    {
+        cout << factor[x] << " ";
+        int cnt = 0;
+        int prime = factor[x];
+        while (x > 1 && factor[x] == prime)
+        {
+            x /= factor[x];
+            cnt++;
+        }
+        cout << cnt << endl;
     }
-
 }
 
-int main() {
+int main()
+{
 
     ios_base::sync_with_stdio(false), cin.tie(NULL);
     modifiedSieve();
     int d;
     cin >> d;
     factfast(d);
-
 }
